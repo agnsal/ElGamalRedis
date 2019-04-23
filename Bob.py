f26e4fa5b48c5ea4c5bcaef21edf356a07f92e1c
Agnese Salutari <agneses92@hotmail.it>
2019-04-23 15:55:21 +0200

    Update Bob.py

--- Bob.py
+++ Bob.py
@@ -19,7 +19,7 @@ import ast
 RCh = Redis.RedisChannel()
 print("Bob creates his ElGamal Keys: ")
 BobElGamal = eg.ElGamalEncryption(False, keyFile='Utils/primes50.txt')
-print("Bob connects and registers his Public Keys on the channel:")
+print("Bob connects and registers his Public Key on the channel:")
 RCh.connect()
 RCh.setRedisVariable(varName='BobPublicKey', varValue=str(BobElGamal.getKeys().getPublicKey()))
 print("Bob waits for Alice's messages from the channel:")
